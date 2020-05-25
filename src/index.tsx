b2c872899d0b3890ee1dd67870aea46d21d7b520
Antony Bartlett <antony.bartlett@innovyze.com>
2020-05-25 14:01:31 +0100

    Add React.

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, RouteComponentProps } from 'react-router-dom';
+
+const App: React.FunctionComponent = () => <div>App works!</div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
