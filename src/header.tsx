142a883f36cb179ec462708193a8087830ba516a
Antony Bartlett <antony.bartlett@innovyze.com>
2020-06-03 22:49:42 +0100

    Remove the other header items.  I might want to get the maps working, but the photos are two far out of date.

--- src/header.tsx
+++ src/header.tsx
@@ -12,15 +12,7 @@ const Header: React.FunctionComponent = () => (
     <Sep></Sep>
     <a href='#Winter'>Winter&nbsp;Course</a>
     <Sep></Sep>
-    <a href='#Running'>Maps</a>
-    <Sep></Sep>
     <a href='#All'>All&nbsp;Races</a>
-    <Sep></Sep>
-    <a href='#Winter2008'>Winter&nbsp;2008&nbsp;Trophies</a>
-    <Sep></Sep>
-    <a href='#Summer2007'>Summer&nbsp;2007&nbsp;Trophies</a>
-    <Sep></Sep>
-    <a href='#Photos'>Older&nbsp;Photos</a>
   </p>
 );
 
